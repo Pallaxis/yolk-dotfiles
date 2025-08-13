fd666044919ddb70f87a765f4ae30b389063495d
Pallaxis <sam_e3u@hotmail.com>
2025-08-13 22:24:20 +1200

    keybinding fix, removing chezmoi from nvim, qt5/6 theme fix

--- eggs/nvim/lua/plugins/treesitter.lua
+++ eggs/nvim/lua/plugins/treesitter.lua
@@ -14,12 +14,6 @@ return {
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
-        disable = function ()
-          -- check if 'filetype' option contains 'chezmoitmpl'
-          if string.find(vim.bo.filetype, 'chezmoitmpl') then
-            return true
-          end
-        end,
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
