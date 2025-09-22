7d082dd34d4f6bb1640cb1f14f762952a4f762f0
Pallaxis <sam_e3u@hotmail.com>
2025-09-22 12:26:09 +1200

    qutebrowser allow autoconfig, hyprland on-created-empty rules, hyprlock now uses wallpaper as background :)

--- eggs/qutebrowser/config.py
+++ eggs/qutebrowser/config.py
@@ -11,7 +11,7 @@
 #   qute://help/settings.html
 
 # Change the argument to True to still load settings configured via autoconfig.yml
-config.load_autoconfig(False)
+config.load_autoconfig(True)
 
 # Automatically start playing `<video>` elements.
 # Type: Bool
