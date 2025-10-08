5a7db09c45c57dddb104bd6893d1c0f2f09b9b9c
Pallaxis <sam_e3u@hotmail.com>
2025-10-09 11:42:33 +1300

    shows active sessions in session switcher now

--- eggs/tmux/scripts/new-session.sh
+++ eggs/tmux/scripts/new-session.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 script_dir="$HOME/.config/tmux/sessions"
-# Get the list of tmuxp sessions
-sessions=$(find "$script_dir" -mindepth 1 -type f -executable -printf "%f\n")
+session_scripts=$(find "$script_dir" -mindepth 1 -type f -executable -printf "%f\n")
+active_sessions+=$(tmux list-sessions | awk -F':' '{print $1}')
+combined_sessions=$(printf "%s\n%s" "$session_scripts" "$active_sessions" | sort -u)
 
 # Use rofi to prompt the user
-chosen=$(echo "$sessions" | rofi -dmenu -i -p "Select session or create new:")
+chosen=$(echo "$combined_sessions" | rofi -dmenu -i -p "Select session or create new:")
 
 # Exit if no selection
 [ -z "$chosen" ] && exit
-
-if echo "$sessions" | grep -qx "$chosen"; then
+if echo "$active_sessions" | grep -qx "$chosen"; then
+    tmux switch-client -t "$chosen"
+elif echo "$session_scripts" | grep -qx "$chosen"; then
     # Runs tmux session setup
     "$script_dir"/"$chosen"
 else
